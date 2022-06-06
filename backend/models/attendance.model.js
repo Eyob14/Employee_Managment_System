990f014c44bba0303fe967fef144ff76630ebf76
Eyob <86522632+Eyob14@users.noreply.github.com>
2022-06-06 07:10:12 +0300

    Delete backend directory
    updated

--- backend/models/attendance.model.js
+++
@@ -1,9 +0,0 @@
-module.exports = (sequelize, Sequelize) => {
-    const Attendance = sequelize.define('attendance', {
-        date: { type: Sequelize.DATEONLY, defaultValue : new Date()},
-        present: {
-            type: Sequelize.BOOLEAN
-        }
-    });
-    return Attendance;
-}
