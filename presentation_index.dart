f41bb7534f2bd17aad526476560df8d2c8fad1b1
bahailu abera <aberabahailu4@gmail.com>
2022-06-06 06:02:28 +0300

    Delete presentation_index.dart

--- presentation_index.dart
+++
@@ -1,8 +0,0 @@
-export 'package:flutter/material.dart';
-export 'auth/auth_index.dart';
-export 'core/core_index.dart';
-export 'attendance/attendance_index.dart';
-export 'profile/profile_index.dart';
-export 'notification/notification_index.dart';
-export 'profile/profile_index.dart';
-export 'routes/routes_index.dart';
